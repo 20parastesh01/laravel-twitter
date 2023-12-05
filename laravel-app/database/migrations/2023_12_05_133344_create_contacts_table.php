686f9ed23cdec0ad80a0a4fdc6af5e65cd20e2c3
parastesh.al2001 <parastesh.al2001@gmail.com>
2023-12-05 05:38:14 -0800

    contacts model created

---
+++ laravel-app/database/migrations/2023_12_05_133344_create_contacts_table.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+}
