407591afda4c2ef896f8c4462570de4c0860534d
parastesh.al2001 <parastesh.al2001@gmail.com>
2023-12-05 01:58:11 -0800

    test controller and route added

---
+++ laravel-app/app/Http/Controllers/TestController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function test()
+    {
+        return 'test is working';
+    }
+
+    public function powNumber(int $number): int
+    {
+        return $number ** $number;
+    }
+}
