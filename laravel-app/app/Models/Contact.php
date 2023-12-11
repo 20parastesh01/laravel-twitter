84197f67365cc976556f9a4ade762878c255fb4c
parastesh.al2001 <parastesh.al2001@gmail.com>
2023-12-11 04:05:20 -0800

    relations added to contact model

--- laravel-app/app/Models/Contact.php
+++ laravel-app/app/Models/Contact.php
@@ -4,10 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Contact extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['first_name', 'last_name', 'email'];
+    protected $fillable = ['first_name', 'last_name', 'email', 'phone_numbers', 'notes', 'addresss'];
+
+    public function phoneNumbers(): HasMany
+    {
+        return $this->hasMany(PhoneNumber::class);
+    }
+
+    public function notes(): MorphMany
+    {
+        return $this->morphMany(Note::class, 'notable');
+    }
+
+    public function addresses(): HasOne
+    {
+        return $this->hasOne(Address::class);
+    }
 }
